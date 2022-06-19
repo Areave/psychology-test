9fdf040a7e4d4dbf3d3f082f07f28a5fdb6c4309
Хохлушин Игорь <just4-spam@mail.ru>
2022-06-19 21:40:03 +0600

    synthax error

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -6,7 +6,7 @@ import MainComponent from "../../comps/mainComponent/mainComponent";
 import LoadingButton from "../../comps/loadingButton/loadingButton";
 import Loader from "../../comps/loader/loader";
 import LoadingTitle from "../../comps/loadingTitle/loadingTitle";
-import PageTitle from "../../comps/PageTitle/pageTitle";
+import PageTitle from "../../comps/pageTitle/pageTitle";
 import WelcomeComp from '../../comps/welcomeComp/welcomeComp'
 import ImageComp from '../../comps/imageComp/imageComp'
 import JokeComp from '../../comps/jokeComp/jokeComp'
@@ -16,7 +16,7 @@ import TestSvg from '../../assets/svg/notification-ico.svg';
 
 const MainPage: React.FC<any> = (props) => {
     return <div className="mainPage">
-        <PageTitle title = {'React Webpack Template'}/>
+        <PageTitle title={'React Webpack Template'}/>
         <MainComponent/>
         <LoadingButton isLoading={props.isLoading} label='loading'/>
         <LoadingTitle title='App is loading:' isLoading={props.isLoading}/>
--- src/pages/page2/page2.tsx
+++ src/pages/page2/page2.tsx
@@ -1,12 +1,12 @@
 import './page2.scss'
 import React from 'react';
 import {useNavigate} from 'react-router'
-import PageTitle from "../../comps/PageTitle/pageTitle";
+import PageTitle from "../../comps/pageTitle/pageTitle";
 
 const Page2 = () => {
     const navigate = useNavigate();
     return <div className="page2">
-        <PageTitle title = {'Page2'}/>
+        <PageTitle title={'Page2'}/>
         <a className='link' onClick={()=>{navigate('/')}}>go to main page</a>
     </div>
 };
